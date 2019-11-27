d4bf68686da76678e78b78c0ff25aa2ec366b158
Kyle Thomson <kjay.thomson@gmail.com>
2019-11-27 11:54:58 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import React from "react"
+
+export const ASYNC_STATES = {
+  IDLE: "IDLE",
+  PENDING: "PENDING",
+  SUCCESS: "SUCCESS",
+  ERROR: "ERROR"
+};
+
+export function useAsync(
+  asyncFn,
+  successTimeout // How long SUCCESS state is active until reverting to IDLE
+) {
+  const [{ status, lastResult, lastError }, updateState] = React.useState({
+    status: ASYNC_STATES.IDLE,
+    lastResult: null,
+		lastError: null 
+  });
+
+  // Thing that calls the async function
+  async function trigger(...args) {
+    updateState(state => ({ ...state, status: ASYNC_STATES.PENDING }));
+    try {
+      const result = await asyncFn(...args);
+      updateState(state => ({
+        ...state,
+        status: ASYNC_STATES.SUCCESS,
+        lastResult: result
+      }));
+      if (successTimeout !== undefined) {
+        // Reset to IDLE state
+        setTimeout(function() {
+          updateState(state => ({ ...state, status: ASYNC_STATES.IDLE }));
+        }, successTimeout);
+      }
+    } catch (error) {
+      updateState(state => ({
+        ...state,
+        status: ASYNC_STATES.ERROR,
+				lastError: error
+      }));
+    }
+  }
+  function reset() {
+    updateState(state => ({
+      ...state,
+      status: ASYNC_STATES.IDLE,
+      lastResult: null,
+			lastError: null
+    }));
+  }
+  return {trigger, status, lastResult, lastError, reset}
+}
+
